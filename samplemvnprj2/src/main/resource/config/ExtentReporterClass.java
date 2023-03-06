46403224bcdadc3780aa5d16454cee14366cedb4
SrikanthChincholi <124526647+SrikanthChincholi@users.noreply.github.com>
2023-03-06 14:00:47 +0530

    Update ExtentReporterClass.java
    remote comments

--- samplemvnprj2/src/main/resource/config/ExtentReporterClass.java
+++ samplemvnprj2/src/main/resource/config/ExtentReporterClass.java
@@ -8,7 +8,9 @@ import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.aventstack.extentreports.reporter.configuration.Theme;
-
+/**
+test remote comment
+**/
 public class ExtentReporterClass {
 
 	public static ExtentSparkReporter htmlreporter;
